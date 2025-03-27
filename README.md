2b2b63d9850d0993fadd8206c8f983f1389a4b9a
Hiram E. Pérez <hiram.eps@gmail.com>
2025-03-26 19:31:39 -0600

    next project

---
+++ README.md
@@ -0,0 +1,2 @@
+# tsdp_2-2025
+Repositorio de temas selectos de programación 2. 2025.
---
+++ my-app/src/app/admin/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste culpa vitae suscipit dolorem recusandae sed et odit, accusantium natus nihil, eveniet animi expedita beatae enim illum corrupti aliquid voluptates porro?
+      </p>
+    </div>
+  );
+}
