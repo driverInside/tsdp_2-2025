1632f1a9ff0aaaa9e889a01dd701b587596a024d
Hiram E. Pérez <hiram.eps@gmail.com>
2025-04-28 07:55:01 -0600

    env variables

---
+++ api_one/src/db/datadase.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+console.log(process.env);
